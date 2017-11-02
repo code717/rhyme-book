1c87866bfcab08a4ef313d8e40a082f3989e875f
code717 <realgame17@gmail.com>
2017-11-02 14:10:50 +0000

    initial commit

---
+++ app/Http/Controllers/RhymeController.php.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class RhymeController extends Controller
+{
+    public function index()
+    {
+        
+    }
+    
+    public function create()
+    {
+        
+    }
+    
+    public function view()
+    {
+        
+    }
+    
+    public function update()
+    {
+        
+    }
+}
---
+++ resources/views/main.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div class="container my-container">
+
+<div class="row text-center">
+<h3>This should be bootstrapped already in container</h3>
+</div>
+
+</div>
+</body>
+
+</html>
+
