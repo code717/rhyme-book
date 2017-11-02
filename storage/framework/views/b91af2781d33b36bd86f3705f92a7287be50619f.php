a59aeabf750262164848f5620fc7c1fef95ca65d
code717 <realgame17@gmail.com>
2017-11-02 15:50:48 +0000

    update

--- storage/framework/views/b91af2781d33b36bd86f3705f92a7287be50619f.php
+++ storage/framework/views/b91af2781d33b36bd86f3705f92a7287be50619f.php
@@ -5,10 +5,64 @@
 <body>
 <div class="container my-container">
 
-<div class="row text-center">
-<h3>This should be bootstrapped already in container</h3>
-</div>
+    <div class="row text-center">
+    <h3>This should be bootstrapped already in container</h3>
+    </div>
+    <div class="jumbotron my-jumbo">
+    <img src="http://wallup.net/wp-content/uploads/2016/12/08/392095-anime-dog-Shiba_Inu.jpg" class="img-thumbnail">
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+        This is panel
+        </div>
+        <div class="panel-body">
+        There will be some nonsense content
+        But bellow you can shake the doge my friend...
+        </div>
+    </div>
+    <div class="row text-center">
+    <img src="https://vignette.wikia.nocookie.net/camphalfbloodroleplay/images/0/05/Doge.png/revision/latest?cb=20140628024222" class="shake shake-hard">
+    </div>
 
+    <div class="well">
+    Living well... I mean literally living in DOM element with "well" class..
+    </div>
+
+    <div class="row vertical-align">
+        <div class="col-md-4 notes-back">
+            <div class="notes-inner shake">
+                <div class="notes-text align-middle text-center">
+                    There some text,<br/>
+                    Attempt to rhyme,<br/>
+                    Skip to next,</br>
+                    It's waste of time<br/>
+                </div>
+            </div>
+        </div>
+    
+        <div class="col-md-4 notes-back">
+            <div class="notes-inner shake shake-hard">
+                <div class="notes-text align-middle text-center">
+                    There some text,<br/>
+                    Attempt to rhyme,<br/>
+                    Skip to next,</br>
+                    It's waste of time<br/>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-4 notes-back">
+            <div class="notes-inner shake shake-opacity">
+                <div class="notes-text align-middle text-center">
+                    There some text,<br/>
+                    Attempt to rhyme,<br/>
+                    Skip to next,</br>
+                    It's waste of time<br/>
+                </div>
+            </div>
+        </div>
+    
+    </div>
 </div>
 </body>
 
